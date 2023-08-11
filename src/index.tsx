4787109aa4d7eb91cbd89311d9bfec78ea0170a4
secret-dev87 <dev.bruce.87@gmail.com>
2023-08-11 05:54:21 -0400

    updated ts config setting

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,8 @@ import App from "./App";
 
 const container = document.createElement("div");
 document.body.appendChild(container);
-document.body.style.cssText = "margin:0;padding:0;border: 1px solid #ADB1F2;border-radius: 12px;box-shadow: 2px 2px 10px #cccccc;";
+document.body.style.cssText =
+  "margin:0;padding:0;border: 1px solid #ADB1F2;border-radius: 12px;box-shadow: 2px 2px 10px #cccccc;";
 
 const root = createRoot(container);
 
